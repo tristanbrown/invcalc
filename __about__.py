d1156a935aa06b4209b8fd8f42979859a9ded595
tristanbrown <brown.tristan.r@gmail.com>
2020-04-20 20:40:01 -0700

    Initialize project structure

---
+++ __about__.py
@@ -0,0 +1,7 @@
+"""About invcalc"""
+__name__ = 'invcalc'
+__version__ = '0.0.1'
+__author__ = "Tristan R. Brown",
+__email__ = "brown.tristan.r@gmail.com",
+__desc__ = "A simple investment calculator.",
+__license__ = "Private",
